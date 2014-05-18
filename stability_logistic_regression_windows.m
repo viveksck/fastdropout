8e8f6803fef1cfa655e500a8d4e98c9f6b965e64
Vivek Kulkarni <vvkulkarni@cs.stonybrook.edu>
2014-05-18 13:25:26 -0400

    Adding code for windows and baseball data sets

---
+++ stability_logistic_regression_windows.m
@@ -0,0 +1,11 @@
+addpath(genpath('binaryLRloss'));
+load('windowsgold.mat');
+% Make sure we allways have the same split of training and test data
+rand('seed', 0)
+C = cvpartition(y, 'kfold',2);
+Xtest = X(C.test(1),:);
+ytest = y(C.test(1));
+Xtrain = X(C.test(2),:);
+ytrain = y(C.test(2));
+output= run_stability_logistic_regression_experiment(Xtrain, ytrain, Xtest, ytest, [0.0, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8], 20);
+save('LogisticRegressionStabilityWindowsFine', 'output')
